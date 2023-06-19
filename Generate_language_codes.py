aec0d4739632064330bb5bf6363ba530e0d261af
Abhishek Bhardwaj <abhishek02bhardwaj.er@gmail.com>
2023-06-19 11:00:25 +0530

    Added the changes suggested by Adam W

---
+++ Generate_language_codes.py
@@ -0,0 +1,36 @@
+import requests
+import re
+import csv
+
+url = "https://en.wikipedia.org/w/index.php?title=List_of_Wikipedias&action=edit&section=9"
+response = requests.get(url)
+if response.status_code == 200:
+    content = response.text
+
+    # Extract language codes using regular expressions
+    pattern = r"\{\{WP7\|([^\|\}]+)"
+    language_codes = re.findall(pattern, content)
+
+    # Generate six combinations for each language code
+    combinations = []
+    for code in language_codes:
+        combinations.append(code)
+        combinations.append(code + "-N")
+        combinations.append(code + "-n")
+        combinations.append(code + "-0")
+        combinations.append(code + "-1")
+        combinations.append(code + "-2")
+        combinations.append(code + "-3")
+        combinations.append(code + "-4")
+        combinations.append(code + "-5")
+
+    # Create a CSV file and write the language codes
+    output_file = "language_codes.csv"
+    with open(output_file, "w", encoding="utf-8", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(["Language Code"])  # Write header row
+        writer.writerows([[code] for code in combinations])
+
+    print("CSV file created successfully:", output_file)
+else:
+    print("Failed to retrieve the Wikipedia page.")
--- translatorsProficiencyDataByUserTemplate.py
+++ translatorsProficiencyDataByUserTemplate.py
@@ -2,7 +2,15 @@ import requests
 import re
 import csv
 
-allowed_languages = ['aa', 'ab', 'ace', 'ady', 'af', 'ak', 'als', 'alt', 'am', 'ami', 'an', 'ang', 'ar', 'arc', 'ary', 'arz', 'as', 'ast', 'atj', 'av', 'avk', 'awa', 'ay', 'az', 'azb', 'ba', 'ban', 'bar', 'bat-smg', 'bcl', 'be', 'be-tarask', 'bg', 'bh', 'bi', 'bjn', 'blk', 'bm', 'bn', 'bo', 'bpy', 'br', 'bs', 'bug', 'bxr', 'ca', 'cbk-zam', 'cdo', 'ce', 'ceb', 'ch', 'cho', 'chr', 'chy', 'ckb', 'co', 'cr', 'crh', 'cs', 'csb', 'cu', 'cv', 'cy', 'da', 'dag', 'de', 'din', 'diq', 'dsb', 'dty', 'dv', 'dz', 'ee', 'el', 'eml', 'en', 'eo', 'es', 'et', 'eu', 'ext', 'fa', 'ff', 'fi', 'fiu-vro', 'fj', 'fo', 'gur', 'fr', 'frp', 'frr', 'fur', 'fy', 'ga', 'gag', 'gan', 'gcr', 'gd', 'gl', 'glk', 'gn', 'gom', 'gor', 'got', 'gu', 'guw', 'gv', 'ha', 'hak', 'haw', 'he', 'hi', 'hif', 'ho', 'hr', 'hsb', 'ht', 'hu', 'hy', 'hyw', 'hz', 'ia', 'id', 'ie', 'ig', 'ii', 'ik', 'ilo', 'inh', 'io', 'is', 'it', 'iu', 'ja', 'jam', 'jbo', 'jv', 'ka', 'kaa', 'kab', 'kbd', 'kbp', 'kcg', 'kg', 'ki', 'kj', 'kk', 'kl', 'km', 'kn', 'ko', 'koi', 'kr', 'krc', 'ks', 'ksh', 'ku', 'kv', 'kw', 'ky', 'la', 'lad', 'lb', 'lbe', 'lez', 'lfn', 'lg', 'li', 'lij', 'lld', 'lmo', 'ln', 'lo', 'lrc', 'lt', 'ltg', 'lv', 'mad', 'mai', 'map-bms', 'mdf', 'mg', 'mh', 'mhr', 'mi', 'min', 'mk', 'ml', 'mn', 'mni', 'mnw', 'mr', 'mrj', 'ms', 'mt', 'mus', 'mwl', 'my', 'myv', 'mzn', 'na', 'nah', 'nap', 'nds', 'nds-nl', 'ne', 'new', 'ng', 'nia', 'nl', 'nn', 'no', 'nov', 'nqo', 'nrm', 'nso', 'nv', 'ny', 'oc', 'olo', 'om', 'or', 'os', 'pa', 'pag', 'pam', 'pap', 'pcd', 'pcm', 'pdc', 'pfl', 'pi', 'pih', 'pl', 'pms', 'pnb', 'pnt', 'ps', 'pt', 'pwn', 'qu', 'rm', 'rmy', 'rn', 'ro', 'roa-rup', 'roa-tara', 'ru', 'rue', 'rw', 'sa', 'sah', 'sat', 'sc', 'scn', 'sco', 'sd', 'se', 'sg', 'sh', 'shi', 'shn', 'si', 'simple', 'sk', 'skr', 'sl', 'smn', 'sm', 'sn', 'so', 'sq', 'sr', 'srn', 'ss', 'st', 'stq', 'su', 'sv', 'sw', 'szl', 'szy', 'ta', 'tay', 'tcy', 'te', 'tet', 'tg', 'th', 'ti', 'tk', 'tl', 'tn', 'to', 'tpi', 'tr', 'trv', 'ts', 'tt', 'tum', 'tw', 'ty', 'tyv', 'udm', 'ug', 'uk', 'ur', 'uz', 've', 'vec', 'vep', 'vi', 'vls', 'vo', 'wa', 'war', 'guc', 'wo', 'wuu', 'xal', 'xh', 'xmf', 'yi', 'yo', 'za', 'zea', 'zh', 'zh-classical', 'zh-min-nan', 'zh-yue', 'zu']
+
+def get_allowed_languages_from_csv(csv_file):
+    allowed_languages = []
+    with open(csv_file, 'r', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        next(reader)  # Skip header row if present
+        for row in reader:
+            allowed_languages.append(row[0])
+    return allowed_languages
 
 
 def get_user_titles_with_babel_from_csv(csv_file):
@@ -15,57 +23,87 @@ def get_user_titles_with_babel_from_csv(csv_file):
     return titles[0:]
 
 
-def find_babel_languages(title):
+def extract_language_codes(template_text):
+    code_pattern = r"(?<=\|)([^\|\[\]]+)(?=\|)"
+    languages = re.findall(code_pattern, template_text)
+    first_element = re.search(r"^[^\|]+", template_text)
+    last_element = re.search(r"[^\|]+$", template_text)
+
+    if first_element:
+        first_element = first_element.group()
+        languages.append(re.sub(r"^\['", '', first_element))
+
+    if last_element:
+        last_element = last_element.group()
+        last_element = re.sub(r"'\]$", '', last_element)
+        if last_element:
+            languages.append(last_element)
+
+    return languages
+
+
+def fetch_content(url, params):
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        return response.text
+    return None
+
+
+def parse_babel_templates(content, allowed_languages):
+    babel_templates = re.findall(r"\{\{User(?![^:}]*?:)(?:[^}]*?)[^\w}](.*?)\}\}", content, re.IGNORECASE)
+    language_claims = []
+    if babel_templates:
+        for babel_template in babel_templates:
+            user_text = str(babel_template)
+            user_languages = extract_language_codes(user_text)
+            if 'translator' in user_text.lower():
+                continue
+            valid_user_languages = set()
+            for lang in user_languages:
+                sub_languages = lang.split('|')
+                for sub_lang in sub_languages:
+                    sub_lang = sub_lang.strip()
+                    sub_lang = sub_lang.rstrip('\n')
+                    if sub_lang in allowed_languages:
+                        valid_user_languages.add(sub_lang)
+
+            language_claims.extend(valid_user_languages)
+
+    return language_claims
+
+
+def find_babel_languages(title, allowed_languages):
     url = "https://en.wikipedia.org/w/index.php"
     params = {
         "title": title,
         "action": "raw"
     }
 
-    response = requests.get(url, params=params)
-    if response.status_code == 200:
-        content = response.text
-        user_templates = re.findall(r"\{\{User(?![^:}]*?:)(?:[^}]*?)[^\w}](.*?)\}\}", content, re.IGNORECASE)
-
-        language_claims = []
-        if user_templates:
-            valid_user_languages = []  # Initialize an empty list
-            for lang in user_templates:
-                if '-' in lang:
-                    language_code, proficiency = lang.rsplit('-', 1)
-                    if language_code in allowed_languages:
-                        valid_user_languages.append(lang)  # Append to the list
-                else:
-                    if lang in allowed_languages:
-                        valid_user_languages.append(lang)  # Append to the list
-
-            language_claims.extend(valid_user_languages)
+    content = fetch_content(url, params)
+    if content:
+        language_claims = parse_babel_templates(content, allowed_languages)
         return title, language_claims
-
     return title, []
 
 
-# CSV file path
-csv_file = 'usernames.csv'
+def create_user_language_csv(csv_file, output_file):
+    allowed_languages = get_allowed_languages_from_csv('language_codes.csv')
+    titles = get_user_titles_with_babel_from_csv(csv_file)
+    results = [find_babel_languages(title, allowed_languages) for title in titles]
+    filtered_results = [(title, languages) for title, languages in results if languages]
 
-# Fetch user titles with Babel templates from CSV
-titles = get_user_titles_with_babel_from_csv(csv_file)
+    with open(output_file, 'w', encoding='utf-8', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Username", "Language"])
 
-# Process Babel languages for each user
-results = [find_babel_languages(title) for title in titles]
+        for title, languages in filtered_results:
+            language_string = '[' + ', '.join([f"'{lang}'" for lang in languages]) + ']'
+            writer.writerow([title, language_string])
 
-# Filter results to exclude users with empty valid_user_languages
-filtered_results = [(title, languages) for title, languages in results if languages]
+    print("CSV file created successfully:", output_file)
 
-# Create a CSV file and write the filtered results
-output_file = 'user_languages_user_template.csv.csv'
 
-with open(output_file, 'w', encoding='utf-8', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(["Username", "Language"])  # Write header row
-
-    for title, languages in filtered_results:
-        language_string = '[' + ', '.join([f"'{lang}'" for lang in languages]) + ']'
-        writer.writerow([title, language_string])
-
-print("CSV file created successfully:", output_file)
+if __name__ == '__main__':
+    csv_file = 'usernames.csv'
+    output_file = 'user_languages_user_template.csv'
+    create_user_language_csv(csv_file, output_file)
